26141a4e393ce7b0db6a3896b772f8c1a1de167e
rizalmab <rizalmahadiab@gmail.com>
2022-08-07 19:15:59 +0800

    Worked

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .json({ app: 'Natours', message: 'Hello from the server side' });
+});
+
+app.post('/', (req, res) => {});
+
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
