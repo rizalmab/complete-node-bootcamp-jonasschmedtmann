00f4ac9b29cceccffa170c9b47a452e7c12a15cd
rizalmab <rizalmahadiab@gmail.com>
2022-08-25 09:09:11 +0800

    Worked on CRUD operations

--- 4-natours/after-section-06-rizal/server.js
+++ 4-natours/after-section-06-rizal/server.js
@@ -13,49 +13,6 @@ mongoose
   })
   .then(() => console.log('DB connection successful'));
 
-const tourSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true
-  },
-  rating: {
-    type: Number,
-    default: 4.5
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price']
-  }
-});
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Forest Hiker',
-  rating: 4.7,
-  price: 497
-});
-
-const testTour3 = new Tour({
-  name: 'Middle Eastern Journey',
-  rating: 3.0,
-  price: 500
-});
-
-// testTour
-//   .save()
-//   .then(doc => console.log(doc))
-//   .catch(err => {
-//     console.log('Error:', err);
-//   });
-
-testTour3
-  .save()
-  .then(doc => console.log(doc))
-  .catch(err => {
-    console.log('Error:', err);
-  });
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
